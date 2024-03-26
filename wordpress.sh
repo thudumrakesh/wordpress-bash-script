42f122f312bc384088b83a341f9d30298ebb4233
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-26 15:56:34 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo yum -y install docker
-sudo yum -y start docker
-sudo yum -y enable docker
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
-cd 
 sudo yum -y install git
 git clone https://github.com/thudumrakesh/wordpress-docker-compose.git
 cd wordpress-docker-compose
